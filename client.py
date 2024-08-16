c09edb70a687f7fb5618d5a5af39b8b908858b5a
Wilson <wwilson008@gmail.com>
2024-08-16 19:39:18 +0800

    @@ client.py[modify]

--- client.py
+++ client.py
@@ -3,10 +3,10 @@ import grpc
 from server.proto import notification_pb2_grpc, notification_pb2
 
 async def run():
-    async with grpc.aio.insecure_channel('localhost:50051') as channel:
+    async with grpc.aio.insecure_channel('localhost:59380') as channel:
         stub = notification_pb2_grpc.NotificationServiceStub(channel)
         response = await stub.SendEmail(notification_pb2.EmailSendRequest(sender="test", subject="test", message="test"))
-        print(f"Greeter client received: {response.message}")
+        print(f"notification client received: {response.message}")
 
 if __name__ == '__main__':
     asyncio.run(run())
